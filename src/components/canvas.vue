479dcce5464358532622eee2ae935d35cb4b3303
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-09 20:21:54 +0530

    render

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -1,46 +1,57 @@
 <script setup>
+import { onMounted } from "vue";
 import * as THREE from "three";
 
-// get the canvas element
-const canvas = document.querySelector("canvas.webgl");
+function renderCanvas() {
+  // get the canvas element
+  const canvas = document.querySelector("canvas.webgl");
 
-// To see something we need 4 elements
-/**
- * Scene
- * Object
- * Camera
- * Renderer
- */
+  // To see something we need 4 elements
+  /**
+   * Scene
+   * Object
+   * Camera
+   * Renderer
+   */
 
-// Scene
-const scene = new THREE.Scene();
+  // Scene
+  const scene = new THREE.Scene();
 
-// Object
-/**
- * A visible object in three is called a mesh
- * Mesh is a combination of Geometry and Materials
- */
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
+  // Object
+  /**
+   * A visible object in three is called a mesh
+   * Mesh is a combination of Geometry and Materials
+   */
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+  const mesh = new THREE.Mesh(geometry, material);
 
-// add it to the scene
-scene.add(mesh);
+  // add it to the scene
+  scene.add(mesh);
 
-// for aspect ratio
-const sizes = { height: 800, width: 800 };
+  // for aspect ratio
+  const sizes = { height: 800, width: 800 };
 
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.height / sizes.width);
+  // Camera
+  const camera = new THREE.PerspectiveCamera(75, sizes.height / sizes.width);
 
-// add to scene
-scene.add(camera);
+  // add to scene
+  scene.add(camera);
 
-// renderer
-const renderer = new THREE.WebGLRenderer({ canvas });
+  // renderer
+  const renderer = new THREE.WebGLRenderer({ canvas });
 
-// resize the renderer
-renderer.setSize(sizes.width, sizes.height);
+  // resize the renderer
+  renderer.setSize(sizes.width, sizes.height);
+
+  // render
+  renderer.render(scene, camera);
+}
+
+// hooks
+onMounted(() => {
+  renderCanvas();
+});
 </script>
 
 <template>
