ae35c88b8e8eced2f8d2daee5bb67fe1cfa8086b
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-09 20:11:19 +0530

    Mesh, camera

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -19,6 +19,13 @@ const scene = new THREE.Scene();
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// add it to the scene
+screen.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera();
 </script>
 
 <template>
