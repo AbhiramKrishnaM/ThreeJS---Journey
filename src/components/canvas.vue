cb2976a26c039f67e4f4f3c330607218049323cb
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-01-23 08:32:31 +0530

    added space

--- src/components/canvas.vue
+++ src/components/canvas.vue
@@ -23,6 +23,7 @@ function renderCanvas() {
    * Mesh is a combination of Geometry and Materials
    */
   const geometry = new THREE.BoxGeometry(1, 1, 1);
+
   const material = new THREE.MeshBasicMaterial({
     color: 0xff0000,
   });
